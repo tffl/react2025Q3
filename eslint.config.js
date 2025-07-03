0da6dcb480aac9b4b0989c3ed78523c305e0e999
Natasha <natalyamerr@gmail.com>
2025-07-03 23:50:47 +0300

    chore: set eslint, prettier and husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,106 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import testingLibrary from "eslint-plugin-testing-library";
+import jestDom from "eslint-plugin-jest-dom";
+import vitest from "@vitest/eslint-plugin";
+import importPlugin from "eslint-plugin-import";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  { ignores: ["dist", "coverage"] },
   {
-    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      importPlugin.flatConfigs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat["jsx-runtime"],
+      reactHooks.configs["recommended-latest"],
+      jsxA11y.flatConfigs.recommended,
+      eslintConfigPrettier,
     ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      "react-refresh": reactRefresh,
+      "testing-library": testingLibrary,
+      "jest-dom": jestDom,
+      vitest: vitest,
+    },
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "@typescript-eslint/consistent-type-exports": "error",
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/no-inferrable-types": "error",
+      "@typescript-eslint/array-type": "error",
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          "newlines-between": "always",
+          pathGroups: [
+            {
+              pattern: "@/**",
+              group: "internal",
+            },
+          ],
+          alphabetize: {
+            order: "asc",
+          },
+        },
+      ],
     },
   },
-])
+  {
+    files: ["**/*.test.{ts,tsx}"],
+    ...testingLibrary.configs["flat/react"],
+    ...jestDom.configs["flat/recommended"],
+    ...vitest.configs.recommended,
+  },
+  {
+    files: ["src/vite-env.d.ts"],
+    rules: {
+      "@typescript-eslint/consistent-type-definitions": "off",
+    },
+  },
+  {
+    files: ["src/components/ui/**/*"],
+    rules: {
+      "react-refresh/only-export-components": "off",
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        typescript: true,
+      },
+    },
+  },
+);

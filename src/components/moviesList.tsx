7555d5452837c0a8d73e9d4d3ce0a518d9586898
Natasha <natalyamerr@gmail.com>
2025-07-14 02:53:38 +0300

    feat: create moviesList component

---
+++ src/components/moviesList.tsx
@@ -0,0 +1,12 @@
+import type { MoviePoster } from '../api/api';
+import MovieCard from './movieCard';
+
+export default function MoviesList({ movies }: { movies: MoviePoster[] }) {
+    return (
+        <div className="movies-list">
+            {movies.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+            ))}
+        </div>
+    );
+}

1b998180b8ef418a81d99649bdd73f4d187b0cd9
Natasha <natalyamerr@gmail.com>
2025-07-14 02:17:22 +0300

    feat: create searchBar component

---
+++ src/components/searchBar.tsx
@@ -0,0 +1,25 @@
+import type { ChangeEvent } from 'react';
+
+interface searchBarStates {
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onSubmit: () => void;
+}
+
+function SearchBar({ value, onChange, onSubmit }: searchBarStates) {
+  return (
+    <form className="search-form" onSubmit={e => (e.preventDefault(), onSubmit())}>
+
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search movies..."
+        value={value}
+        onChange={onChange} />
+
+      <button type="submit" className="search-btn">Search</button>
+    </form>
+  );
+}
+
+export default SearchBar;

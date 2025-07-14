209c4cbc6b87ff2cff22fce8bd83a0ea7cecbc63
Natasha <natalyamerr@gmail.com>
2025-07-14 23:24:55 +0300

    feat: create errorBoundary component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import { Component } from 'react';
 import MovieApp from './components/movieApp';
 
 class App extends Component {
+  state = { shouldThrow: false };
+
   render() {
+    if (this.state.shouldThrow) {
+      throw new Error('Test error');
+    }
+
     return (
       <div className="App">
+        <button onClick={() => this.setState({ shouldThrow: true })} className="crash-btn">C̴̖̼̬̃̏̓̍Ṝ̸A̷͔͑̋͘S̶̩̼͓̪̄̀́H̵̹̦̺̺͚̎ ̸̲͓̖̠̝̒̀̈̉͘T̷̮̾̇̚ͅḦ̴̤́È̷͙͝͠ ̷̪̂͌̂̓̇Ä̸̲̤̍P̸̤̮͉͛͆̀͊P̶̡͕̯̍</button>
         <h1>Movies search</h1>
         <MovieApp />
       </div>

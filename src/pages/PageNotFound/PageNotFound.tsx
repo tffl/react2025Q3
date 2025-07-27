f19c7b4338ac970426a4a9c83b2b5fe912655cc8
Natasha <natalyamerr@gmail.com>
2025-07-27 21:40:48 +0300

    feat: create not-found page

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,9 +1,21 @@
 import { Header } from "../../ui-components/Header/Header";
+import "./PageNotFound.css";
 
 export const PageNotFound = () => {
   return (
     <>
       <Header />
+      <div className="page-not-found">
+        <h2>
+          4̴̨̛͎̙0̸̤̥̊͑́4̸̞͉͈̤̈́́̓̾̔̒ ̷̡̰̻̩̔|̴͎͙͔͖͕̇̒͆̾ ̸͙̭͗P̴̥͖̀̾͝À̴̞̠̱̫̠͓̀͊̽̍̉͠G̸̮̥̟͉͈̝̜̈̉E̷̡̺̦̫̦͔̓͜ ̷̱̦͑N̷̜͓̭̩̬̻̲͋̒̏̊̑̌͆O̵͚͋̈́̀̀̔̑͗T̷̖̘͆̐ ̴̧̘̤̫̕͝͠F̴̳͙͑͘͜O̶̳̩̹̗͍͑̐͐͐̂́͝ͅŪ̶͙͉͓̦̠Ń̶̼͚̈́̚D̶̛̻̯̼͌͜ ̶̜͈̄̌̋̒̄|̷̛̭̤̎̄͂̽̍͒ ̴̨͙̱̱͕̭̘̋̈́̆̍̽̕͠4̵̡̡͉͖͇̓̋͗0̷̣̣̖̩̩̀͆͒̕4̸̺̯̲̥̲̹̍̌̋̽͆
+        </h2>
+        <button
+          className="reload-btn"
+          onClick={() => (window.location.href = "/")}
+        >
+          Back home
+        </button>
+      </div>
     </>
   );
 };

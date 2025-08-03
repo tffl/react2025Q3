94dcac5a31a2c255e20427ab565a36eacfbc913c
Natasha <natalyamerr@gmail.com>
2025-08-04 02:26:41 +0300

    test: fix movies tests with provider

--- src/components/MovieCard/MovieCard.test.tsx
+++ src/components/MovieCard/MovieCard.test.tsx
@@ -1,28 +1,35 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
 import { describe, it, expect, beforeEach } from "vitest";
 
+import { store } from "../../store/store";
 import { mockMovies } from "../../test-utils/mockMovies";
+
 
 import MovieCard from "./MovieCard";
 
 describe("MovieCard", () => {
-    const movie = mockMovies[0];
+  const movie = mockMovies[0];
 
-    beforeEach(() => {
-        render(<MovieCard movie={movie} />);
-    });
+  beforeEach(() => {
+    render(
+      <Provider store={store}>
+        <MovieCard movie={movie} />
+      </Provider>
+    );
+  });
 
-    it("render correct movie image", () => {
-        const img = screen.getByRole("img", { name: movie.title });
-        expect(img.getAttribute("src")).toBe(movie.posterUrl);
-        expect(img.getAttribute("alt")).toBe(movie.title);
-    });
+  it("render correct movie image", () => {
+    const img = screen.getByRole("img", { name: movie.title });
+    expect(img.getAttribute("src")).toBe(movie.posterUrl);
+    expect(img.getAttribute("alt")).toBe(movie.title);
+  });
 
-    it("render movie title", () => {
-        expect(screen.getByText(movie.title)).toBeTruthy();
-    });
+  it("render movie title", () => {
+    expect(screen.getByText(movie.title)).toBeTruthy();
+  });
 
-    it("render movie description", () => {
-        expect(screen.getByText(movie.overview)).toBeTruthy();
-    });
+  it("render movie description", () => {
+    expect(screen.getByText(movie.overview)).toBeTruthy();
+  });
 });
--- src/components/MoviesList/MoviesList.test.tsx
+++ src/components/MoviesList/MoviesList.test.tsx
@@ -1,21 +1,31 @@
 import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
 import { describe, it, expect } from "vitest";
 
+import { store } from "../../store/store";
 import { mockMovies } from "../../test-utils/mockMovies";
+
 
 import MoviesList from "./MoviesList";
 
 describe("MoviesList", () => {
-    it("render movies list", () => {
-        const { container } = render(<MoviesList movies={mockMovies} />);
-        const cards = container.querySelectorAll(".movie-card");
-        expect(cards.length).toBe(mockMovies.length);
-    });
+  it("render movies list", () => {
+    const { container } = render(
+      <Provider store={store}>
+        <MoviesList movies={mockMovies} />
+      </Provider>
+    );
+    const cards = container.querySelectorAll(".movie-card");
+    expect(cards.length).toBe(mockMovies.length);
+  });
 
-    it("not render anything if movies array is empty", () => {
-        const { container } = render(<MoviesList movies={[]} />);
-        const cards = container.querySelectorAll(".movie-card");
-        expect(cards.length).toBe(0);
-    });
-
+  it("not render anything if movies array is empty", () => {
+    const { container } = render(
+      <Provider store={store}>
+        <MoviesList movies={[]} />
+      </Provider>
+    );
+    const cards = container.querySelectorAll(".movie-card");
+    expect(cards.length).toBe(0);
+  });
 });

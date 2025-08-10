e92ff151d6d535b06493646513ebd984c5360db6
Natasha <natalyamerr@gmail.com>
2025-08-10 21:16:46 +0300

    refactor: remove error boundary button

--- src/pages/PageHome/PageHome.test.tsx
+++ src/pages/PageHome/PageHome.test.tsx
@@ -1,13 +1,10 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
 import { describe, test, expect, beforeEach } from "vitest";
 
-import { PageHome, crashButtonText } from "./PageHome";
+import { PageHome } from "./PageHome";
 
-let crashButton: HTMLElement;
 let pageTitle: HTMLElement;
-const user = userEvent.setup();
 
 const testSetup = () => {
   render(
@@ -20,19 +17,10 @@ const testSetup = () => {
 describe("PageHome", () => {
   beforeEach(() => {
     testSetup();
-    crashButton = screen.getByRole("button", { name: crashButtonText });
     pageTitle = screen.getByRole("heading", { name: "Movies search" });
   });
 
   test("render page title", () => {
     expect(pageTitle).toBeTruthy();
-  });
-
-  test("render crash button", () => {
-    expect(crashButton).toBeTruthy();
-  });
-
-  test("crash button throws error", async () => {
-    await expect(() => user.click(crashButton)).rejects.toThrow("Test error");
   });
 });

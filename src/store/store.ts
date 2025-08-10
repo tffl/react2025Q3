c477d0da0fb110d030e8bd3267b64d96078cef73
Natasha <natalyamerr@gmail.com>
2025-08-10 23:31:46 +0300

    refactor: integrate API slice into Redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { movieApi } from '../api/api';
+
 import selectedMoviesReducer from './selectedMoviesSlice';
 
 export const store = configureStore({
   reducer: {
     selectedMovies: selectedMoviesReducer,
+    [movieApi.reducerPath]: movieApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(movieApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
